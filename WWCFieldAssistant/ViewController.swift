2bbb6db7ba0b3ca66f93351dc6b6a8d09d1fbb66
AustinDev16 <austin.blaser@gmail.com>
2016-12-10 17:30:12 -0700

    Initial Commit

---
+++ WWCFieldAssistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWCFieldAssistant
+//
+//  Created by Austin Blaser on 12/10/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
