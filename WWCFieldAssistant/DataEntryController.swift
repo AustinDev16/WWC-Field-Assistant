a63a9ff54adab227d87c52a9978ad6849f7d9fcc
AustinDev16 <austin.blaser@gmail.com>
2016-12-12 14:23:45 -0700

    Added DataEntryController

---
+++ WWCFieldAssistant/DataEntryController.swift
@@ -0,0 +1,12 @@
+//
+//  DataEntryController.swift
+//  WWCFieldAssistant
+//
+//  Created by Austin Blaser on 12/12/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import Foundation
+class DataEntryController {
+    
+}
