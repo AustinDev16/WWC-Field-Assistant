cda53ae380cee39e763e6b592e6fc5aa1ab02d8d
AustinDev16 <austin.blaser@gmail.com>
2016-12-12 17:50:20 -0700

    Fleshed out functions for the Data Controller.

--- WWCFieldAssistant/DataEntryController.swift
+++ WWCFieldAssistant/DataEntryController.swift
@@ -7,31 +7,58 @@
 //
 
 import Foundation
+import UIKit
+
 class DataEntryController {
     let well: Well
     
     init(well: Well){
+        // This may also be done as a fetch from Core Data to then initialize all corresponding data entries and photos.
         self.well = well
     }
     
     /// Adds a newly completed data entry to the corresponding well
-    func addDataEntry(){
+    func addDataEntry(dateCollected: NSDate,
+                      fieldNotes: String,
+                      make: String,
+                      meterReading: Double,
+                      model: String,
+                      multiplier: Multiplier,
+                      serialNumber: String,
+                      unitType: MeterUnitType){
         
+        guard let newEntry = DataEntry(dateCollected: dateCollected, fieldNotes: fieldNotes, make: make, meterReading: meterReading, model: model, multiplier: multiplier, serialNumber: serialNumber, unitType: unitType, well: self.well) else { return }
+        
+        self.well.addToDataEntries(newEntry)
+        PersistenceController.saveToPersistedStore()
     }
     
     /// Deletes a data entry from a well
-    func deleteDataEntry(){
-        
+    func deleteDataEntry(entryToDelete: DataEntry){
+        guard let moc = entryToDelete.managedObjectContext else { return }
+        self.well.removeFromDataEntries(entryToDelete)
+        moc.delete(entryToDelete)
+        PersistenceController.saveToPersistedStore()
     }
     
     /// Adds a field photo, name, and comments to a data entry
-    func addFieldPhotoToDataEntry(){
-        
+    func addFieldPhotoToDataEntry(dateTaken: NSDate,
+                                  comment: String,
+                                  name: String,
+                                  dataEntry: DataEntry,
+                                  image: UIImage){
+        guard let newFieldPhoto = FieldPhoto(dateTaken: dateTaken, comment: comment, name: name, dataEntry: dataEntry, image: image) else { return }
+        dataEntry.addToFieldPhotos(newFieldPhoto)
+        PersistenceController.saveToPersistedStore()
     }
     
     /// Deletes a field photo from a data entry
-    func deleteFieldPhotoFromEntry(){
-        
+    func deleteFieldPhotoFromEntry(fieldPhotoToDelete: FieldPhoto){
+        guard let moc = fieldPhotoToDelete.managedObjectContext else { return }
+        let dataEntry = fieldPhotoToDelete.dataEntry
+        dataEntry.removeFromFieldPhotos(fieldPhotoToDelete)
+        moc.delete(fieldPhotoToDelete)
+        PersistenceController.saveToPersistedStore()
     }
     
     
