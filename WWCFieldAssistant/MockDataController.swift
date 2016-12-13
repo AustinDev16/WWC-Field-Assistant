05d3c17c101f784a5daf656ceb2612a55828a8b3
AustinDev16 <austin.blaser@gmail.com>
2016-12-12 18:14:30 -0700

    Added mock data skeleton.

---
+++ WWCFieldAssistant/MockDataController.swift
@@ -0,0 +1,25 @@
+//
+//  MockData.swift
+//  WWCFieldAssistant
+//
+//  Created by Austin Blaser on 12/12/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import Foundation
+
+class MockDataController {
+    
+    static func populateDistricts(){
+        
+    }
+    
+    static func populateWells(){
+        
+    }
+    
+    static func populateDataEntries(){
+        
+    }
+    
+}
