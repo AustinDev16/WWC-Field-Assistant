bc1984067da8175aaa4e11acd1d3ddd1dac199b8
AustinDev16 <austin.blaser@gmail.com>
2016-12-29 19:56:26 -0700

    Added a controller that will run fetches on core data objects.

---
+++ WWCFieldAssistant/AppDataController.swift
@@ -0,0 +1,51 @@
+//
+//  AppDataController.swift
+//  WWCFieldAssistant
+//
+//  Created by Austin Blaser on 12/29/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class AppDataController {
+    static let shared = AppDataController()
+    
+    var districts: [District] = []
+    
+    /// Fetches all districts from persistent store
+    func fetchDistricts(){
+        let fetchRequest: NSFetchRequest<District> = District.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        if #available(iOS 10.0, *) {
+            let moc = CoreDataStack.context
+            do {
+                let results = try moc.fetch(fetchRequest)
+                self.districts = results
+            } catch {
+                print("Error fetching districts, iOS10 \(error.localizedDescription)")
+            }
+            
+        } else {
+            // Fallback on earlier versions
+            let moc = Stack.sharedStack.managedObjectContext
+            do {
+                let results = try moc.fetch(fetchRequest)
+                self.districts = results
+            } catch {
+                print("Error fetching districts, iOS9 \(error.localizedDescription)")
+            }
+        }
+
+    }
+    
+    /// Creates an instance of DistrictController with a selected district
+    func initiateDistrictController(district: District) -> DistrictController {
+        return DistrictController(district: district)
+    }
+    
+    
+}
