a5589a90cdcdda68cc342f1fe9669a7d34f68349
AustinDev16 <austin.blaser@gmail.com>
2017-02-01 16:18:10 -0700

    Tableview now updates with new field notes; fixed issue where lines showed over the background view when no data is  present.

--- WWCFieldAssistant/FieldNotesSummaryTableViewCell.swift
+++ WWCFieldAssistant/FieldNotesSummaryTableViewCell.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class FieldNotesSummaryTableViewCell: UITableViewCell {
+    // MARK: - Properties
+    var well: Well?
+    var fieldNotes: [FieldNote] {
+        return self.well?.fieldNotes.flatMap{$0 as? FieldNote} ?? []
+    }
+    
+    var tableView: UITableView {
+        if _tableView == nil {
+            _tableView = UITableView()
+        }
+        return _tableView!
+    }
+    
+    var _tableView: UITableView? = nil
     
     func configureCell<T: UIViewController> (parentView: T){
+        let castView = parentView as? WellSummaryTableViewController
+        self.well = castView?.well
         configureTableView(parentView: parentView)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateFieldNote), name: Notification.Name(rawValue: "FieldNoteUpdated"), object: nil)
+    }
+    
+    func updateFieldNote(){
+        self.tableView.reloadData()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     func configureTableView<T: UIViewController>(parentView: T){
-        let tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight =  UITableViewAutomaticDimension
@@ -37,37 +61,40 @@ class FieldNotesSummaryTableViewCell: UITableViewCell {
 extension FieldNotesSummaryTableViewCell: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        if true {
+        if  self.well == nil || self.fieldNotes.count == 0 {
             let view = UIView(frame: self.contentView.frame)
             view.backgroundColor = UIColor.white
             tableView.backgroundView = view
+            tableView.separatorStyle = .none
             let label = UILabel()
             label.text = "No notes available"
             label.textColor = UIColor.lightGray
             label.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(label)
+            view.insertSubview(label, at: 0)
             let centerY = NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0)
             let centerX = NSLayoutConstraint(item: label, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
             view.addConstraints([centerY, centerX])
             return 0
         } else {
+            tableView.separatorStyle = .none
         tableView.backgroundView = nil
         return 1
         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.fieldNotes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = FieldNoteTableViewCell()
-        cell.configureCellWith(date: Date(), note: "text alsdkjf ajsk djakjs djsk fjak fue feisj fjjsij tjhejsk tistu sjti af ajf s fjiej")
+        let fieldNote = self.fieldNotes[indexPath.row]
+        cell.configureCellWith(date: fieldNote.date as Date,note: fieldNote.note)
         return cell
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 30
+        return 60
     }
     
 }
